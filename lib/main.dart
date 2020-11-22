6ac10293e256aae620fea1426c6f0929a1673f32
EduardoM-GO <68598652+EduardoM-GO@users.noreply.github.com>
2020-11-22 19:44:52 -0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:chat_com_firebase/screen/chatscreen.dart';
+//import 'package:chat_com_firebase/screen/chatscreen.dart';
+import 'package:chat_com_firebase/AuthScreen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -17,7 +18,8 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: ChatScreen(),
+      home: AuthScreen(),
+      //home: ChatScreen(),
     );
   }
 }
--- lib/screen/chatscreen.dart
+++ lib/screen/chatscreen.dart
@@ -3,31 +3,79 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
+  TextEditingController _mensagem = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection("Mensagens").snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: CircularProgressIndicator(),
+    MediaQueryData deviceInfo = MediaQuery.of(context);
+
+    Widget Lista() {
+      return Container(
+        height: MediaQuery.of(context).size.height - 70,
+        child: StreamBuilder(
+          stream:
+              FirebaseFirestore.instance.collection("Mensagens").orderBy("createdAt").snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            final documents = snapshot.data.docs;
+            return ListView.builder(
+              scrollDirection: Axis.vertical,
+              itemCount: documents.length,
+              itemBuilder: (ctx, i) => Container(
+                padding: EdgeInsets.all(8),
+                child: Text(documents[i]['msg']),
+              ),
             );
-          }
-          final documents = snapshot.data.docs;
-          return ListView.builder(
-            itemCount: documents.length,
-            itemBuilder: (ctx, i) => Container(
-              padding: EdgeInsets.all(8),
-              child: Text(documents[i]['msg']),
+          },
+        ),
+      );
+    }
+
+    Widget EscreveMensagem() {
+      return Container(
+        width: MediaQuery.of(context).size.width,
+        child: TextFormField(
+          keyboardType: TextInputType.text,
+          decoration: InputDecoration(
+            labelText: "Mensagem",
+            labelStyle: TextStyle(
+              color: Colors.blue,
             ),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {},
-      ),
+            suffix: GestureDetector(
+              child: Icon(
+                Icons.send,
+                color: Colors.blueAccent,
+              ),
+              onTap: () {
+                FirebaseFirestore.instance.collection("Mensagens").add({
+                  'msg': _mensagem.text,
+                  'Usuario': "Eduardo",
+                  'createdAt': Timestamp.now(),
+                });
+                _mensagem.text = "";
+              },
+            ),
+          ),
+          controller: _mensagem,
+          textAlign: TextAlign.left,
+          style: TextStyle(color: Colors.black, fontSize: 20.0),
+        ),
+      );
+    }
+
+    return Scaffold(
+      body: SingleChildScrollView(
+          physics: ScrollPhysics(),
+          child: Column(
+            children: [
+              Lista(),
+              EscreveMensagem(),
+            ],
+          )),
     );
   }
 }
